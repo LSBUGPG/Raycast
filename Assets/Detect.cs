1960cdfab10b1688cb9a81d22fa9596ea8e17b83
Paul Sinnett <paul.sinnett@gmail.com>
2017-03-28 16:01:29 +0100

    example raycast program
    program to demonstrate how to use raycasting

---
+++ Assets/Detect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detect : MonoBehaviour {
+
+    public float distance = 1.0f;
+
+	void Update () {
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        Color color = Color.green;
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            color = Color.red;
+            Debug.Log(hit.collider.tag);
+        }
+        Debug.DrawRay(ray.origin, ray.direction * distance, color);
+	}
+}
